7a66133545562883202a7cd83bfde1152899c052
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-18 20:07:03 +0530

    Fixed caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -41,6 +41,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       else await axios.post("/api/issues", data);
 
       router.push("/");
+      router.refresh();
     } catch (error) {
       setError("An unexpected error has occured.");
     }
