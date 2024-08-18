39054a79fff26bc3bff9cdf1c06cd3206adf92b4
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-18 11:34:55 +0530

    Disable ssr for loading MDE

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <Box>
+      <Skeleton height="5rem" />
+      <Skeleton height="20rem" />
+    </Box>
+  );
 };
 
 export default loading;
