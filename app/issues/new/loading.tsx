762a75318df37f707eec63474406b0f546fba473
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-18 20:41:55 +0530

    Improve the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,13 +1,3 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-const loading = () => {
-  return (
-    <Box>
-      <Skeleton height="5rem" />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default loading;
+export default IssueFormSkeleton;
