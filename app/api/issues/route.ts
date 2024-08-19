f6180d9d276f2350c4ac6c5df6cc34f0dc875dd4
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-19 13:14:50 +0530

    Secured the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,13 @@
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/ValidationSchemas";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
