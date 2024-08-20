af96e8edc29c762091980f2d2c7a076d403f1afe
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-20 22:03:49 +0530

    Sorting issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -14,25 +14,27 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status as Status)
-    ? { status: searchParams.status }
-    : undefined;
-
-  const orderByFilter = searchParams.orderBy
-    ? { [searchParams.orderBy]: "asc" as const }
-    : undefined;
-
-  const issues = await prisma.issue.findMany({
-    where: status,
-    orderBy: orderByFilter,
-  });
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status as Status)
+    ? { status: searchParams.status }
+    : undefined;
+
+  const orderByFilter =
+    searchParams.orderBy &&
+    columns.map((column) => column.value).includes(searchParams.orderBy)
+      ? { [searchParams.orderBy]: "asc" }
+      : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: status,
+    orderBy: orderByFilter,
+  });
 
   return (
     <div>
