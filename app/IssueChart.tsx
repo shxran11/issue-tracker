7ac492370e5a839aacf718f31ddbbb083020a149
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-08-22 09:48:36 +0530

    Built the issue chart component

---
+++ app/IssueChart.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Card } from "@radix-ui/themes";
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+  Tooltip,
+} from "recharts";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data = [
+    { label: "Open", value: open },
+    { label: "In-progress", value: inProgress },
+    { label: "Closed", value: closed },
+  ];
+
+  console.log(data); // Check if data is logged correctly
+
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Tooltip />
+          <Bar
+            barSize={50}
+            dataKey="value"
+            style={{ fill: "var(--accent-8)" }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default IssueChart;
